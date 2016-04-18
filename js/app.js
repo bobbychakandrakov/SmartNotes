77a2b1e8afd9098709cd5b1bc07ff17ddd74d080
Boyan <bobilincho@gmail.com>
2016-04-18 12:36:48 +0300

    adding templates

--- js/app.js
+++ js/app.js
@@ -1,7 +1,12 @@
 var app = angular.module('SmartNotes', ['ngRoute']);
 app.config(function ($routeProvider) {
     $routeProvider.when('/',{
-
+        templateUrl:"templates/authentication/login.html",
+        controller:"LoginController"
     })
+    .when('/register',{
+        templateUrl:"templates/authentication/register.html",
+        controller:"RegisterController"
+     })
     .otherwise({redirectTo:'/'});
 });
